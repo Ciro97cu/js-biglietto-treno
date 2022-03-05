3a82d89db442b0315082c183a2c70bff31d3c37b
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-05 12:06:45 +0100

    reworked script

--- javascript/script.js
+++ javascript/script.js
@@ -6,8 +6,16 @@ il prezzo del tuo biglietto`);
 const age = prompt("Quanti anni hai?", "Es. 25");
 // constant kilometers to do
 const kmToDo = prompt("Quanti kilometri vuoi percorrere?", "Es. 10");
+
+// age and km in the html
+document.getElementById("your_age").innerText = `${age}anni`;
+document.getElementById("your_km").innerText = `${kmToDo}Km`;
+
 // ticket price depending on the kilometers
-let priceTicket = kmToDo * 0.21;
+let priceTicket = (kmToDo * 0.21).toFixed(2);
+// price discount per age
+let priceOver65 = (priceTicket - (priceTicket * 0.40)).toFixed(2);
+let priceUnder18 = (priceTicket - (priceTicket * 0.20)).toFixed(2);
 
 /*check if age and kmToDo is in the 
 correct format using if*/
@@ -21,27 +29,16 @@ if (isNaN(age)) {
     console.log("Hai dimenticato di compilare un campo o inserito 0");
     document.getElementById("customer_price").innerText = "Hai dimenticato di compilare un campo o inserito 0"
 } else {
-    /*once checked the format, we can calculate
-    the eventual discount and than print the price
+    /*once checked the format, print the price
     in the console and in the html*/
     if (age < 18) {
-        priceTicket -= priceTicket * 0.20;
-        priceTicket = priceTicket.toFixed(2);
-        console.log(`Sei minorenne hai diritto ad un
-    20% di sconto, perciò il prezzo finale del tuo biglietto sarà di
-    ${priceTicket}€`)
-        document.getElementById("customer_price").innerText = `Sei minorenne hai diritto ad un 20% di sconto, perciò il prezzo finale del tuo biglietto sarà di ${priceTicket}€`;
+        console.log(`20% di sconto minorenni, il prezzo del tuo biglietto è di ${priceUnder18}€`)
+        document.getElementById("customer_price").innerText = `20% di sconto minorenni, il prezzo del tuo biglietto è di ${priceUnder18}€`;
     } else if (age > 65) {
-        priceTicket -= priceTicket * 0.40;
-        priceTicket = priceTicket.toFixed(2);
-        console.log(`Sei over 65 hai diritto ad un
-    40% di sconto, perciò il prezzo finale del tuo biglietto sarà di
-    ${priceTicket}€`)
-        document.getElementById("customer_price").innerText = `Sei over 65 hai diritto ad un 40% di sconto, perciò il prezzo finale del tuo biglietto sarà di ${priceTicket}€`;
+        console.log(`40% di sconto over 65, il prezzo del tuo biglietto è di ${priceOver65}€`)
+        document.getElementById("customer_price").innerText = `40% di sconto over 65, il prezzo del tuo biglietto è di ${priceOver65}€`;
     } else {
-        priceTicket = priceTicket.toFixed(2);
-        console.log(`Il prezzo del tuo biglietto
-    è di: ${priceTicket}€`)
+        console.log(`Il prezzo del tuo biglietto è di: ${priceTicket}€`)
         document.getElementById("customer_price").innerText = `Il prezzo del tuo biglietto è di: ${priceTicket}€`;
     }
 }
